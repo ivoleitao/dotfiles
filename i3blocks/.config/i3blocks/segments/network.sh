441588015800f3fda4c3d41e5b05204a9f21782d
Ivo Leitão <ivo.leitao@celfocus.com>
2015-04-06 13:53:04 +0100

    added systemd services, fonts and network segment. changed the ncmpcpp configuration to account for i3 scratchpad configuration

--- i3blocks/.config/i3blocks/segments/network.sh
+++ i3blocks/.config/i3blocks/segments/network.sh
@@ -2,75 +2,61 @@
 
 source "$(dirname $0)/config.sh"
 
-format_wireless() {
-    local value=$1
-    local icon=""
-
-    if [ $value -le 5 ]
-    then
-        icon="$BATTERY_ICON_CRITICAL"
-    elif [ $value -le 10 ] 
-    then
-        icon="$BATTERY_ICON_VERY_LOW"
-    elif [ $value -le 20 ] 
-    then
-        icon="$BATTERY_ICON_LOW"
-    elif [ $value -le 40 ]
-    then
-        icon="$BATTERY_ICON_MEDIUM"
-    elif [ $value -le 60 ]
-    then
-        icon="$BATTERY_ICON_HIGH"
-    elif [ $value -le 80 ]
-    then
-        icon="$BATTERY_ICON_VERY_HIGH"
-    elif [ $value -le 100 ]
-    then
-        icon="$BATTERY_ICON_MAX"
-    elif [ $value -eq 100 ]
-    then
-        icon="$BATTERY_ICON_FULL"
-    fi
-
-    printf "<span foreground='$BATTERY_ICON_COLOR'>$icon</span> %-3s" "$value%"
+format_wired() {
+    local ip=$1
+    printf "<span foreground='$NETWORK_ICON_COLOR'>$NETWORK_ICON_WIRED</span> %s" "$ip"
 }
 
-format_wired() {
-    local value=$1
+format_wireless() {
+    local ip=$1
+    local ssid=$2
+    local quality=$3
     local icon=""
 
-    if [ $value -le 5 ]
-    then
-        icon="$BATTERY_ICON_CRITICAL"
-    elif [ $value -le 10 ] 
-    then
-        icon="$BATTERY_ICON_VERY_LOW"
-    elif [ $value -le 20 ] 
-    then
-        icon="$BATTERY_ICON_LOW"
-    elif [ $value -le 40 ]
-    then
-        icon="$BATTERY_ICON_MEDIUM"
-    elif [ $value -le 60 ]
-    then
-        icon="$BATTERY_ICON_HIGH"
-    elif [ $value -le 80 ]
-    then
-        icon="$BATTERY_ICON_VERY_HIGH"
-    elif [ $value -le 100 ]
-    then
-        icon="$BATTERY_ICON_MAX"
-    elif [ $value -eq 100 ]
-    then
-        icon="$BATTERY_ICON_FULL"
+    if [ $quality -le 20 ]; then
+        icon="$NETWORK_ICON_WIRELESS_VERY_LOW"
+    elif [ "$quality" -le "40" ]; then
+        icon="$NETWORK_ICON_WIRELESS_LOW"
+    elif [ $quality -le 60 ]; then
+        icon="$NETWORK_ICON_WIRELESS_MEDIUM"
+    elif [ $quality -le 80 ]; then
+        icon="$NETWORK_ICON_WIRELESS_HIGH"
+    elif [ $quality -le 100 ]; then
+        icon="$NETWORK_ICON_WIRELESS_VERY_HIGH"
     fi
 
-    printf "<span foreground='$BATTERY_ICON_COLOR'>$icon</span> %-3s" "$value%"
+    printf "<span foreground='$NETWORK_ICON_COLOR'>$icon</span> (%-3s at %s) %s" "$quality%" "$ssid" "$ip"
+}
+
+format_none() {
+    printf "<span foreground='$NETWORK_ICON_COLOR'>$NETWORK_ICON_NONE</span>"
 }
 
 panel_network() {
-
-    echo "$(format_network "$value")"
+    local net=$(ip addr)
+    local eth_state=$(echo "$net" | awk '/'$NETWORK_INTERFACE_WIRED':/  { print $9 }')
+    local wlan_state=$(echo "$net" | awk '/'$NETWORK_INTERFACE_WIRELESS':/ { print $9 }')
+    local interface=""
+    if [[ "$eth_state" == "UP" && "$wlan_state" == "UP" ]]; then
+        interface=$(ip route | awk '/default/ { print $5 }')
+    elif [ "$eth_state" == "UP" ]; then
+        interface=$NETWORK_INTERFACE_WIRED
+    elif [ "$wlan_state" == "UP" ]; then
+        interface=$NETWORK_INTERFACE_WIRELESS
+    fi
+    
+    local ip=""
+    if [[ "$interface" == "$NETWORK_INTERFACE_WIRED" ]]; then
+        ip=$(echo "$net" | awk '/inet.*'$NETWORK_INTERFACE_WIRED'/  { print $2 }')
+        echo "$(format_wiredi "$ip")"
+    elif [ "$interface" == "$NETWORK_INTERFACE_WIRELESS" ]; then
+        ip=$(echo "$net" | awk '/inet.*'$NETWORK_INTERFACE_WIRELESS'/  { print $2 }')
+        local essid=$(/sbin/iwgetid --raw)
+        local quality=$(awk 'NR==3 {printf("%.0f\n",$3*10/7)}' /proc/net/wireless)
+        echo "$(format_wireless "$ip" "$essid" "$quality")"
+    else
+        echo "$(format_none)"
+    fi
 }
 
 echo $(panel_network)
